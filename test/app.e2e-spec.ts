25500fb3d503a369e4ae37ac6cf8797c9e3551b3
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-02 19:55:43 +0000

    feat: added test for signup
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
-import { AppModule } from "@/app.module"
-import { INestApplication, ValidationPipe } from "@nestjs/common"
+import * as pactum from 'pactum'
 import { Test } from "@nestjs/testing"
+import { INestApplication, ValidationPipe } from "@nestjs/common"
+
+import { AuthDto } from "../src/auth/dto"
+import { AppModule } from "../src/app.module"
+import { PrismaService } from "../src/prisma/prisma.service"
 
 describe('App e2e', () => {
   let app: INestApplication
+  let prisma: PrismaService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,11 +21,35 @@ describe('App e2e', () => {
     }))
 
     await app.init()
+    await app.listen(3333)
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDB()
   })
 
   afterAll(() => {
     app.close()
   })
 
-  it.todo('should pass')
+  describe("Auth", () => {
+    describe("signup", () => {
+      it('should signup', () => {
+        const dto: AuthDto = {
+          email: 'yaw@mailinator.com',
+          name: 'Yaw Kyei',
+          password: "password"
+        }
+        return pactum.spec().post('http://localhost:3333/auth/register').withBody(dto).expectStatus(201)
+      })
+    })
+
+    describe("login", () => {
+    })
+  })
+
+  describe("User", () => {
+  })
+
+  describe("Employee", () => {
+  })
 })
