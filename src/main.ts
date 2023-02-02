32434c44201612e926266cd2c5d56b02e3b2af97
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-02 19:59:40 +0000

    feat: added documentation
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,14 @@ async function bootstrap() {
     whitelist: true
   }))
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Leave system API')
+    .setDescription('The leave system API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
