f6fb468f611335b520ea5f8d01d8ba1212651d9c
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-02 19:58:12 +0000

    feat: added documentation
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator'
 
 export class AuthDto {
+    @ApiProperty({
+        description: 'The name of the user',
+        example: "John Doe"
+    })
     @IsString()
     @IsNotEmpty()
     name: string
 
+    @ApiProperty({
+        description: "The email address of the user",
+        example: "john.doe@gmail.com"
+    })
     @IsString()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        description: "The password of the user",
+        example: "pass@123"
+    })
     @IsString()
     @IsNotEmpty()
     password: string;
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator'
 
 export class LoginDto {
+    @ApiProperty({
+        description: 'The email of the user',
+        example: 'john.doe@gmail.com'
+    })
     @IsString()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        description: 'The password of the user',
+        example: 'pass@123'
+    })
     @IsString()
     @IsNotEmpty()
     password: string;
