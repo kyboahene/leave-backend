6717d0d935c1bbf427095f80de36db0139804d77
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-02 20:01:00 +0000

    little changes
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 import { AuthDto, LoginDto } from '@/auth/dto';
 import { User } from '@/interfaces/user.interface';
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 
 @Injectable()
