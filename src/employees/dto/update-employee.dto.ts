73a60b841c8fab0ff8648491a56e30e8e77afa72
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-06 12:17:57 +0000

    feat: added a dto for updating employee
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

---
+++ src/employees/dto/update-employee.dto.ts
@@ -0,0 +1,28 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateEmployeeDto {
+    @ApiProperty({
+        description: 'The type of the employee',
+        example: "JUNIOR"
+    })
+    @IsString()
+    @IsOptional()
+    employee_type?: "JUNIOR" | "SENIOR"
+
+    @ApiProperty({
+        description: 'The region id of the employee',
+        example: 1
+    })
+    @IsNumber()
+    @IsOptional()
+    region_id?: number
+
+    @IsNumber()
+    @IsOptional()
+    district_id?: number
+
+    @IsNumber()
+    @IsOptional()
+    user_id?: number
+}
