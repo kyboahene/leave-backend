d2140e248360018621547aaaeb55baed02743fa0
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-06 12:15:57 +0000

    feat: completed the crud endpoints for the employee table
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 
+import { JwtGuard } from '@/auth/guard';
+import { GetUser } from '@/auth/decorator';
+import { Employee } from './entities/employee.entity';
+import { EmployeesService } from './employees.service';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@ApiTags('Employee')
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) { }
+
+  @UseGuards(JwtGuard)
+  @Post()
+  @ApiCreatedResponse({
+    description: "Created employee object as response",
+    type: Employee
+  })
+  @ApiBadRequestResponse({
+    description: 'Employee cannot be created. Try again!'
+  })
+  create(@GetUser('division_id') divisionId: number, @Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeesService.create(divisionId, createEmployeeDto);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get()
+  @ApiCreatedResponse({
+    description: "Created an array of employee object(s) as response",
+    type: [Employee]
+  })
+  @ApiBadRequestResponse({
+    description: 'Employees cannot be retrieved. Try again!'
+  })
+  findAll(@GetUser('division_id') divisionId: number) {
+    return this.employeesService.findAll(divisionId);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeesService.findOne(+id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+    return this.employeesService.update(+id, updateEmployeeDto);
+  }
+
+  @UseGuards(JwtGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeesService.remove(+id);
+  }
+}
