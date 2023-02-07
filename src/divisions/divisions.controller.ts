0f413eea01b86a4767f0be2cabe8d4b7b14be539
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-07 15:09:04 +0000

    feat: added authentication to the endpoints
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/divisions/divisions.controller.ts
+++ src/divisions/divisions.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { JwtGuard } from '@/auth/guard';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 
 import { DivisionsService } from './divisions.service';
 import { CreateDivisionDto, UpdateDivisionDto } from './dto';
@@ -7,26 +8,31 @@ import { CreateDivisionDto, UpdateDivisionDto } from './dto';
 export class DivisionsController {
   constructor(private readonly divisionsService: DivisionsService) { }
 
+  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createDivisionDto: CreateDivisionDto) {
     return this.divisionsService.create(createDivisionDto);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll() {
     return this.divisionsService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.divisionsService.findOne(+id);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDivisionDto: UpdateDivisionDto) {
     return this.divisionsService.update(+id, updateDivisionDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.divisionsService.remove(+id);
