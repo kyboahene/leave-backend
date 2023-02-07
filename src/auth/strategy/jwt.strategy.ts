39c5598d32b306cf89dc75c3a1f5ac3fea7f01fe
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-07 15:09:58 +0000

    fix: inputted the correct jwt secret
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,16 @@
-import { PrismaService } from '@/prisma/prisma.service';
+import { ExtractJwt, Strategy } from "passport-jwt";
+
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: config.get('JWT_TOKEN'),
+            secretOrKey: config.get('JWT_SECRET'),
         });
     }
 
