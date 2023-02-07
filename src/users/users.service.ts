c7956896129e1fb99f678534d550fd6f6de02c5d
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-07 15:17:08 +0000

    feat: completed the services on user table
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
+import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) { }
+
+    findAll(divisionId: number) {
+        return this.prisma.employee.findMany({
+            where: {
+                division_id: divisionId
+            },
+            select: {
+                user: true
+            }
+        })
+    }
+
+    findOne(id: number) {
+        return this.prisma.user.findUnique({ where: { id } });
+    }
+
+    update(id: number, data: UserDto) {
+        return this.prisma.user.update({ where: { id }, data });
+    }
+
+    remove(id: number) {
+        return this.prisma.user.delete({ where: { id } });
+    }
+}
