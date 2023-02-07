b5152ef8c9364f909477daf363b98c029fadf632
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-07 15:12:23 +0000

    feat: added role entity
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,33 @@
-export class Role {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Role {
+    @ApiProperty({
+        description: 'The id of the role',
+        example: 1
+    })
+    id: number
+
+    @ApiProperty({
+        description: 'The name of the role',
+        example: "Super Admin"
+    })
+    name: string
+
+    @ApiProperty({
+        description: 'The access level of the role',
+        example: 6
+    })
+    access_level: number
+
+    @ApiProperty({
+        description: 'The date and time it was created',
+        example: ''
+    })
+    created_at: Date
+
+    @ApiProperty({
+        description: 'The date and time it was updated',
+        example: ''
+    })
+    updated_at: Date
+}
