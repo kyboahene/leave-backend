4afcf0db8df9bac445663e4a35e685ab5edc2f66
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-02-08 08:18:18 +0000

    refactor: url changes
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
+  SwaggerModule.setup('/docs', app, document);
 
   await app.listen(process.env.PORT);
 }
