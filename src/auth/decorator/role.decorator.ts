4c4880905745819da862295a6f3f50712e07760e
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-02 07:19:58 +0000

    feat: created role decorator
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/auth/decorator/role.decorator.ts
+++ src/auth/decorator/role.decorator.ts
@@ -1,7 +1,3 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (
-    (roles: number[]): any => {
-        SetMetadata('roles', roles)
-    }
-);
+export const Roles = (...roles: number[]) => SetMetadata('roles', roles);
