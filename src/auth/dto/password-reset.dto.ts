9b57d8f5a621fba7c1ed58aeb0dad77e0cbaca52
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-08 09:25:00 +0000

    feat: added is not empty class
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/auth/dto/password-reset.dto.ts
+++ src/auth/dto/password-reset.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class PasswordResetDto {
     @IsString()
+    @IsNotEmpty()
     password: string
 
     @IsString()
+    @IsNotEmpty()
     confirmPassword: string
 }
