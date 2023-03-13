f9efe50705aa3ca0381f7864206673e7512237c3
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-13 08:35:18 +0000

    feat: completed all entities
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/districts/entities/district.entity.ts
+++ src/districts/entities/district.entity.ts
@@ -1,1 +1,34 @@
-export class District {}
+import { Region } from "@/regions/entities/region.entity";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class District {
+    @ApiProperty({
+        description: 'The id of the district',
+        example: 1
+    })
+    id: number
+
+    @ApiProperty({
+        description: 'The name of the district',
+        example: "John Doe"
+    })
+    name: string
+
+    @ApiProperty({
+        description: 'The region of the district',
+        example: "Central"
+    })
+    region: Region
+
+    @ApiProperty({
+        description: 'The date and time the district details was last updated',
+        example: "2020-18-01"
+    })
+    updated_at: Date
+
+    @ApiProperty({
+        description: 'The time and date the district was created',
+        example: "2020-18-01"
+    })
+    created_at: Date
+}
