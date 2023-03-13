6f08bacb02f9106c84914e323b529c4974343529
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-13 08:28:24 +0000

    add: added tags and response for documentation
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/districts/districts.controller.ts
+++ src/districts/districts.controller.ts
@@ -1,38 +1,60 @@
 import { GetUser } from '@/auth/decorator';
 import { JwtGuard } from '@/auth/guard';
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { DistrictsService } from './districts.service';
 import { CreateDistrictDto } from './dto/create-district.dto';
 import { UpdateDistrictDto } from './dto/update-district.dto';
+import { District } from './entities/district.entity';
 
+@ApiTags('District')
 @Controller('districts')
 export class DistrictsController {
   constructor(private readonly districtsService: DistrictsService) { }
-
-  // @UseGuards(JwtGuard)
+  @ApiCreatedResponse({
+    description: "Creates district object as response",
+    type: District
+  })
+  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createDistrictDto: CreateDistrictDto) {
     return this.districtsService.create(createDistrictDto);
   }
 
-  // @UseGuards(JwtGuard)
+  @ApiCreatedResponse({
+    description: "Returns an array of district object(s) as response",
+    type: [District]
+  })
+  @UseGuards(JwtGuard)
   @Get()
   findAll(@GetUser('division_id') divisionId: number) {
     return this.districtsService.findAll(divisionId);
   }
 
+  @ApiCreatedResponse({
+    description: "Returns a district object as response",
+    type: District
+  })
   @UseGuards(JwtGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.districtsService.findOne(+id);
   }
 
+  @ApiCreatedResponse({
+    description: "Returns the updated district object as response",
+    type: District
+  })
   @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDistrictDto: UpdateDistrictDto) {
     return this.districtsService.update(+id, updateDistrictDto);
   }
 
+  @ApiCreatedResponse({
+    description: "Returns a successful message",
+    type: "District deleted successful"
+  })
   @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
