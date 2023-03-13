970d6665b32109808d2bc3b1060ab9761ca4512f
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-13 08:27:33 +0000

    add: added  api tag for documentation
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,9 +1,11 @@
 import { GetUser } from '@/auth/decorator';
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
+@ApiTags('Comment')
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
