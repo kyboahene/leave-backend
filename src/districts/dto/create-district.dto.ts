34515dd4ebc3c3ec170d6a6f8d9acf97e040adcd
Yaw Kyei Boahene <kyeiyaw437@gmail.com>
2023-03-13 08:35:03 +0000

    feat: added documentation
    Signed-off-by: Yaw Kyei Boahene <kyeiyaw437@gmail.com>

--- src/districts/dto/create-district.dto.ts
+++ src/districts/dto/create-district.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNumber, IsString } from "class-validator"
 
 export class CreateDistrictDto {
+    @ApiProperty({
+        description: "The name of the district",
+        example: "Asikuma"
+    })
     @IsString()
     name: string
 
+    @ApiProperty({
+        description: "The id of the region in which the district is found",
+        example: 1
+    })
     @IsNumber()
     region_id: number
 }
